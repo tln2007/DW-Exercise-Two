9137a30a6e11aac23606d0f247c38f68c23f9752
Tasnim Nahar <tln2007@nyu.edu>
2022-09-20 09:38:35 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import Article from "./pages/article";
+import List from "./pages/list";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <List />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
