19807edee12fdc062ba7e94104a84c05b0ce9e6b
Tasnim Nahar <tln2007@nyu.edu>
2022-09-27 09:35:00 -0400

    updated title and used find method

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,19 +1,27 @@
+import { findAllByAltText } from "@testing-library/react";
 import React from "react";
+import { useParams } from "react-router";
+import Data from '../components/data'
 
 function Article () {
-    const backgroundImageUrl = 
-        "url('https://64.media.tumblr.com/659ad69128231083423ccf335567c095/tumblr_oujxtk2mZB1wvke5ro3_1280.jpg')"
+    const { id } = useParams();
+
+    const articleData = Data.find((article) => (
+        article.id === id
+    ))
+    
+    console.log(articleData)
     return (
         <main>
             <header 
                 className="article--header"
                 style={{ 
-                    backgroundImage: backgroundImageUrl,
+                    backgroundImage: "",
                     backgroundSize: "cover",
                 }}
             >
                 <div className="article--header--wrapper">
-                    <h1>Article Title</h1>
+                    <h1>{articleData.title}</h1>
                     <p>Wednesday, August 22, 2019</p>
                     <p>Note that this is the article blurb. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut lab.</p>
                 </div>
